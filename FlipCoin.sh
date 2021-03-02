62390a7872170c8126990cff3ffc132647bab1cc
bhavana <bhavanapoudala9@gmail.com>
2021-03-02 17:51:34 +0530

    Added: Number of times Tail and Head occurred

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,13 +1,27 @@
 
 #!/bin/bash -x
 
-outcome=$((RANDOM%2))
 
-if [ $outcome -eq 1 ]
-then
-	echo HEADS
-else
-	echo TAILS
-fi
+heads=0
+tails=0
+count=0
+while [ $count -lt 100 ]
+do
+    coin=$((RANDOM%2))
+    if [ $coin -eq 1 ]
+    then
+        heads=$(($heads+1))
+    else
+
+        tails=$(($tails+1))
+    
+    fi
+((count++))
+done
+
+
+echo "outcome of heads: " $heads
+echo "outcome of tails: " $tails
+
  
 
